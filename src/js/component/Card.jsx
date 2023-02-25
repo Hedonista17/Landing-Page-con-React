aff834b5d368231daa507bdbfcbc6723d36b6516
Hedonista17 <andreaynereapiza@gmail.com>
2023-02-25 13:12:27 +0000

    Realizado FOOTER Y CARDS DE MOMENTO

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -4,12 +4,12 @@ const Card = (variable) => {
 
     return (
    <div>
-    <div class="card mt-5 mx-3 " style={{width: "20rem"}}>
-  <img src="{variable.imagen}" class="card-img-top" />
+    <div class="card mt-5 mx-5" style={{width: "20rem"}}>
+  <img class="card-img-top" src={variable.imagen}></img>
   <div class="card-body">
     <h5 class="card-title">{variable.titulo}</h5>
     <p class="card-text">{variable.texto}</p>
-    <a href="https://www.youtube.com/watch?v=_jzqsWM3zCQ" target="_blank" class="btn btn-dark">{variable.link}</a>
+    <a href={variable.link} target="_blank" class="btn btn-dark"> Mas informacion 🔗 </a>
   </div>
 </div>
 
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,22 +6,22 @@ import Fotter from "./footer.jsx";
 
 //include images into your bundle
  let cartas = [{
-  imagen: "",
+  imagen: "https://i2.wp.com/cinicosdesinope.com/wp-content/uploads/2015/09/earl-sneed-sinclair-papa-dinosaurios-serie.gif?resize=600%2C497",
   titulo:"¿Sabías que?",
   texto:"El traje de dinosaurio completo está formado por dos partes, un traje de cuerpo entero y la cabeza animatrónica por separado. La cabeza tiene motores que mueven la mandíbula, los labios, los ojos y las cejas. Para tomas de diálogo en primer plano y para la sincronización labial, las cabezas fueron separadas y operadas por un equipo de titiriteros.",
-  link:"Mas informacion >>",
+  link:"https://www.youtube.com/watch?v=_jzqsWM3zCQ&t=8s",
  },
    {
-	imagen:"cffer",
-	titulo:"few",
-	texto:"frfer",
-	link:"rfgerw",
+	imagen:"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/F3D0C75D0523218C9441A8E76ABC5FC0DDBE9F175533177ADABAB0327271E22E/scale?width=506&aspectRatio=2.00&format=jpeg",
+	titulo:"Más que una serie",
+	texto:"En 1993, se llegó a negociar un videojuego de 'Dinosaurios' para Super Nintendo. En aquella época se desarrollaron varios juegos basados en series, pero no tuvieron buena acogida al no tener nada que ver con la serie y solo usaban el nombre durante la moda. Cuando se estaba negociando un acuerdo para 'Dinosaurios', la moda ya había pasado.",
+	link:"https://www.youtube.com/watch?v=0kmqcc70QA0",
    },
    {
-	imagen:"cffer",
-	titulo:"few",
-	texto:"frfer",
-	link:"rfgerw",
+	imagen:"https://m.media-amazon.com/images/I/71LFrI+d-BL._SL1400_.jpg",
+	titulo:"Otras curiosidades",
+	texto:"En varios episodios, se puede ver en la televisión a una banda de rock llamada Lizzard Skizzard. El nombre es una evidente referencia a la clásica banda de rock Lynyrd Skynyrd, pero la banda real tiene mucho más en común con varias de las bandas de los 80-90.",
+	link:"https://www.youtube.com/watch?v=V6kf0vwFj5U",
    }]
 
 //create your first component
@@ -31,12 +31,14 @@ const Home = () => {
 			 <NavBar/>
 		 <div className="container">
             <JumBotron/>
+			<div className="container">
 			<div className = "card-group">
 			 
 			 { cartas.map ((propiedad)=> {
 				return <Card  imagen = {propiedad.imagen} titulo ={propiedad.titulo} texto = {propiedad.texto} link ={propiedad.link} />
 			 })}
 
+			</div>
 			</div>
 		</div>
 		
